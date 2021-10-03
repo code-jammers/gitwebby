89472a298aa29964e5a6e9444e037c82084b94f9
Kasey <kaseyjameshinton@gmail.com>
2021-10-02 23:59:45 -0600

    Set up routes and load web components and data from data folder.

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,10 +1,16 @@
 import { css, customElement, html, LitElement, state } from 'lit-element';
 import { ChangePathEvent, PendingStateEvent, SiteErrorEvent } from './events';
 import navaid from 'navaid';
+import { Repository } from './types/Repository';
+import { Branch } from './types/Branch';
+declare let REPOS: any;
+declare let BRANCHES: any;
 
 @customElement('my-app')
 export class MyAppElement extends LitElement {
   @state() private page: string | undefined;
+  @state() private repositories: Array<Repository> = [];
+  @state() private branches: Array<Branch> = [];
 
   async firstUpdated() {
     this.addEventListener(ChangePathEvent.eventName, (event: ChangePathEvent) => {
@@ -18,21 +24,31 @@ export class MyAppElement extends LitElement {
     const router = navaid();
 
     router.on('/', () => {
-      this.#changePage('');
+      this.#changePage('main');
     });
 
-    router.on('/commits', () => {
-      this.#changePage('commits');
+    router.on('/repositories', () => {
+      this.#changePage('repositories', () => import('./components/git-repositories.js'));
+    });
+
+    router.on('/branches', () => {
+      this.#changePage('branches', () => import('./components/git-branches.js'));
     });
 
     router.listen();
   }
 
   async #lazyLoadExtraComponents() {
-    // try {
-    // } catch (error) {
-    //   console.warn('One or more components failed to load', error);
-    // }
+    try {
+      await import('../data/repos.js');
+      await import('../data/branches.js');
+      console.log(REPOS);
+      console.log(BRANCHES);
+      this.repositories = JSON.parse(JSON.stringify(REPOS));
+      this.branches = JSON.parse(JSON.stringify(BRANCHES));
+    } catch (error) {
+      console.warn('One or more components failed to load', error);
+    }
   }
 
   #changePage(mainPage: string, importFunction?: () => void) {
@@ -67,6 +83,10 @@ export class MyAppElement extends LitElement {
   ];
 
   render() {
-    return html` Web components go here ${this.page}`;
+    return html`
+      <toolbar> </toolbar>
+      <git-repositories .repositories=${this.repositories} ?hidden=${this.page !== 'repositories'} ?active=${this.page === 'repositories'}></git-repositories>
+      <git-branches .branches=${this.branches} ?hidden=${this.page !== 'branches'} ?active=${this.page === 'branches'}></git-branches>
+    `;
   }
 }
