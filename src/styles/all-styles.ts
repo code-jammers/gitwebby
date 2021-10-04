1f772ac4087a412f74a9efa360477485bf5c4df3
Kasey <kaseyjameshinton@gmail.com>
2021-10-03 21:32:28 -0600

    removes unused microtip lib

--- src/styles/all-styles.ts
+++ src/styles/all-styles.ts
@@ -1,6 +1,5 @@
 import GlobalCss from './global';
-import Microtip from './microtip';
 import milligram from './milligram';
 
-const allStyles = [Microtip, milligram, GlobalCss];
+const allStyles = [milligram, GlobalCss];
 export default allStyles;
