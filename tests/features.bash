0c1caa2868ba1e3c4d9aeb4f7d48e0bc351659e1
Korey Hinton <korey.hinton@gmail.com>
2021-10-24 09:30:11 -0400

    file and lp changes
    - file: grep file command output in search for text to detect file type
    - lp: Added simplified code algorithm explanations above code blocks,
    for literate programming documentation
    
    Failing Tests: 3

--- tests/features.bash
+++ tests/features.bash
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-# Run like this:
-#   tests/features.bash
+ss="$_"  # script source
+sd=$(dirname "$ss")  # script dir
+rd=$(dirname "$sd")  # repo dir
 
-for ftr_f in tests/ftr_*.bash
-do
-    ${ftr_f}
-done
+cd "$sd"
+sd=$(pwd)
+cd -
+
+cd "$rd"
+rd=$(pwd)
+cd -
+
+echo
+echo FEATURE TESTS
+echo RUN EACH ftr_\*.bash file IN SCRIPT-SOURCE-DIR/
+echo AND SAVE LITERATE PROGRAMMNIG OUTPUT WITHIN SCRIPT-SOURCE-DIR/../self_doc
+echo ...
+{
+    for ftr_f in $sd/ftr_*.bash
+    do
+        bnm=$(basename $ftr_f)
+        mkdir -p "${rd}/self_doc/tests/${bnm}/"
+        ftr_out=$($ftr_f)
+        echo "$ftr_out" | grep -v '^\s' > "${rd}/self_doc/tests/${bnm}/README"
+        echo "$ftr_out" | grep -i PASS
+        echo "$ftr_out" | grep -i FAIL
+    done
+}
+echo ... DONE
